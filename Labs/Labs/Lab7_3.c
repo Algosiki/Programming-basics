77078a95b6b237c861c0929b64d9267a4187f8c0
Agibalov Ilya Stanislavovich <59418574+RaskolMarm@users.noreply.github.com>
2023-10-30 00:37:57 +0300

    Update Lab7_3.c

--- Labs/Labs/Lab7_3.c
+++ Labs/Labs/Lab7_3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 union HexToBits {
-    unsigned int hexValue;
+    unsigned long long hexValue;
     struct {
         unsigned int bit1 : 1;
         unsigned int bit2 : 1;
@@ -15,9 +15,9 @@ int main() {
     scanf_s("%x", &converter.hexValue);
 
 
-    printf("%s", converter.bits.bit1 ? "вопроизведение вкл \n" : "вопроизведение выкл \n");
-    printf("%s", converter.bits.bit2 ? "пауза вкл \n" : "пауза выкл \n");
-    printf("%s", converter.bits.bit3 ? "запись вкл \n" : "запись выкл \n");
+    printf("%s", converter.bits.bit1 ? "ГўГ®ГЇГ°Г®ГЁГ§ГўГҐГ¤ГҐГ­ГЁГҐ ГўГЄГ« \n" : "ГўГ®ГЇГ°Г®ГЁГ§ГўГҐГ¤ГҐГ­ГЁГҐ ГўГ»ГЄГ« \n");
+    printf("%s", converter.bits.bit2 ? "ГЇГ ГіГ§Г  ГўГЄГ« \n" : "ГЇГ ГіГ§Г  ГўГ»ГЄГ« \n");
+    printf("%s", converter.bits.bit3 ? "Г§Г ГЇГЁГ±Гј ГўГЄГ« \n" : "Г§Г ГЇГЁГ±Гј ГўГ»ГЄГ« \n");
 
     return 0;
 }

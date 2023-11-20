517909f061706a5f4a1dd886babac3d61f7015e9
Agibalov Ilya Stanislavovich <59418574+RaskolMarm@users.noreply.github.com>
2023-11-20 03:03:13 +0300

    Update Lab6.c

--- Labs/Labs/Lab6.c
+++ Labs/Labs/Lab6.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 
 int main() {
-    char* arr = (char*)malloc(sizeof(char) * 4);
+    // 1
+    char* arr[4];
+    arr[0] = 'a';
+    arr[1] = 'b';
+    arr[2] = 'c';
+    arr[3] = 'd';
 
     for (int i = 0; i < 4; ++i) {
-        scanf_s(" %c", &arr[i]);
+        printf("%c ", *(arr + i));
     }
+    printf("\n");
+    // 2
+
+    char* arr2 = (char*)malloc(4 * sizeof(char));
+    arr2[0] = 'a';
+    arr2[1] = 'b';
+    arr2[2] = 'c';
+    arr2[3] = 'd';
 
     for (int i = 0; i < 4; ++i) {
-        printf("%c ", arr[i]);
+        printf("%c ", arr2[i]);
     }
-
-    free(arr);
-
+    free(arr2);
+    
     return 0;
 }

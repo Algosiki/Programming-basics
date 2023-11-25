b86893c626314d8126ff29c14105ebf8e26cc7e2
Agibalov Ilya Stanislavovich <59418574+RaskolMarm@users.noreply.github.com>
2023-11-25 13:46:26 +0300

    Update cTest.c

--- Labs/Labs/Lab11/cTest.c
+++ Labs/Labs/Lab11/cTest.c
@@ -3,24 +3,22 @@
 
 int main() {
     Rectangle rect;
-
-    // Ввод координат точек прямоугольника с консоли
-    printf("%s", "Координаты первой точки: ");
-    scanf_s("%d %d", &rect.x1, &rect.y1);
-    printf("%s", "Координаты второй точки: ");
-    scanf_s("%d %d", &rect.x2, &rect.y2);
-    printf("%s", "Координаты третьей точки: ");
-    scanf_s("%d %d", &rect.x3, &rect.y3);
-    printf("%s", "Координаты четвертой точки: ");
-    scanf_s("%d %d", &rect.x4, &rect.y4);
+    // 1 РєРѕРѕСЂРґРёРЅР°С‚Р°
+    scanf("%d %d", &rect.x1, &rect.y1);
+    // 2 РєРѕРѕСЂРґРёРЅР°С‚Р°
+    scanf("%d %d", &rect.x2, &rect.y2);
+    // 3 РєРѕРѕСЂРґРёРЅР°С‚Р°
+    scanf("%d %d", &rect.x3, &rect.y3);
+    // 4 РєРѕРѕСЂРґРёРЅР°С‚Р°
+    scanf("%d %d", &rect.x4, &rect.y4);
     
-    // Расчет площади и периметра прямоугольника
+    
     int area = calculateArea(rect);
     int perimeter = calculatePerimeter(rect);
 
-    // Вывод результатов
-    printf("Площадь прямоугольника: %d\n", area);
-    printf("Периметр прямоугольника: %d\n", perimeter);
+    
+    printf("%d\n", area);
+    printf("%d\n", perimeter);
 
     return 0;
 }

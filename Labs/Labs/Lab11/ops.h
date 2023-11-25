f83dc19f7958d68ac48a27afc1bef71dc78422b2
Agibalov Ilya Stanislavovich <59418574+RaskolMarm@users.noreply.github.com>
2023-11-25 13:38:39 +0300

    Update ops.h

--- Labs/Labs/Lab11/ops.h
+++ Labs/Labs/Lab11/ops.h
@@ -2,47 +2,15 @@
 #define ops_h
 
 typedef struct {
-    int x1, y1; // Координаты первой точки
-    int x2, y2; // Координаты второй точки
+    int x1, y1;
+    int x2, y2;
     int x3, y3;
     int x4, y4;
 } Rectangle;
 
-int calculateArea(Rectangle rect) {
-    int width = 0, height = 0;
-    if (rect.x1 != rect.x2) {
-         width = (rect.x2 - rect.x1);
-    }
-    else {
-         width = (rect.x2 - rect.x3);
-    }
-    if (rect.y1 != rect.y2) {
-         height = (rect.y2 - rect.y1);
-    }
-    else {
-         height = (rect.y2 - rect.y3);
-    }
-    return width * height < 0 ? -width * height : width * height;
-}
+int calculateArea(Rectangle rect);
 
-int calculatePerimeter(Rectangle rect) {
-    int width = 0, height = 0;
-    if (rect.x1 != rect.x2) {
-         width = (rect.x2 - rect.x1);
-    }
-    else {
-         width = (rect.x2 - rect.x3);
-    }
-    width = width < 0 ? -width : width;
-    if (rect.y1 != rect.y2) {
-         height = (rect.y2 - rect.y1);
-    }
-    else {
-         height = (rect.y2 - rect.y3);
-    }
-    height = height < 0 ? -height : height;
-    return 2 * (width + height);
-}
+int calculatePerimeter(Rectangle rect);
 
 
 #endif
